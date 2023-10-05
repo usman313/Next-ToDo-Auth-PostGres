2f23f7b9b023282ac36b51c86d8861303accde5a
Abbas-Ahsan <138559995+Abbas-Ahsan@users.noreply.github.com>
2023-10-05 12:49:35 +0500

    only get issue pending

--- src/app/api/create-todo-table/route.js
+++ src/app/api/create-todo-table/route.js
@@ -4,7 +4,12 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
   try {
     const result =
-      await sql`CREATE TABLE todo (id serial PRIMARY KEY, title text, task text, status VARCHAR(255));`;
+      await sql`CREATE TABLE todo (
+                  id serial PRIMARY KEY, 
+                  title text, 
+                  task text, 
+                  status VARCHAR(255)
+                );`;
       return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
---
+++ src/app/api/edit-todo/route.js
@@ -0,0 +1,26 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function PUT(request){
+    const{id, title, description, status} = await request.json()
+
+    const result = await sql`UPDATE todo 
+                            SET 
+                                title=${title}, 
+                                task=${description}, 
+                                status=${status}
+                            WHERE 
+                                id=${id};`
+
+    if(result.rowCount > 0){
+        return NextResponse.json({
+            status: 'OK',
+            message: `Task ${title} edited Successfully`
+        }, {status:200})
+    }else{
+        return NextResponse.json({
+            message:'Invalid Parametes',
+            status: 401
+        }, {status: 401})
+    }
+}
